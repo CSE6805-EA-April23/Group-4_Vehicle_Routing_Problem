8abaac9f86972798d7ea9c5f59727e6af9d978ba
Asif Hasan <asif.hasan.chowdhury@g.bracu.ac.bd>
2023-09-17 23:13:19 +0600

    Skeleton Code VRP

---
+++ VehicleRouteStatic.py
@@ -0,0 +1,40 @@
+
+
+def UserInputHandle():
+    print("User Input Handle")
+
+
+
+def graphGeneration():
+    print("Prepare Graph")
+
+
+
+def generateRoute():
+    print("Generate Route")
+
+    
+
+
+def ParentInitilization():
+    print("do parent generate")
+    
+
+
+def FitnessCalculation():
+    print("do fitness calculationt")
+    
+
+
+def Selection():
+    print("do Crossover")
+
+
+
+def Mutation():
+    print("do Mutation")
+
+
+
+def CrossOver():
+    print("do CrossOver")
