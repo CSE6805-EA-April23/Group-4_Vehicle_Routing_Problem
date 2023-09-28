0061c00a38391f83d8aff574e6421f24caafad8d
Asif Hasan <asif.hasan.chowdhury@g.bracu.ac.bd>
2023-09-29 03:29:07 +0600

    gitIgnore Functional

--- filelocation.py
+++
@@ -1,8 +0,0 @@
-def filePath():
-    return 'C:\Genetic Algo\py-ga-VRPTW\data\json\C204.json'
-
-
-
-#This file will be Added to Gitignore 
-#  'E:\GA\py-ga-VRPTW\data\json\C201.json'
-# 'C:\Genetic Algo\py-ga-VRPTW\data\json\C204.json'
