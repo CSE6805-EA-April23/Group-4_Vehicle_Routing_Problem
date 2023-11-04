02738d804def40a44262733793b77442c27ae806
Avijit Biswas <66124696+avi-sudo@users.noreply.github.com>
2023-11-05 03:10:16 +0600

    Fixed a bug

--- Variation2VehicleRouting.py
+++ Variation2VehicleRouting.py
@@ -42,7 +42,7 @@ def print_route(route, instance,merge=False):
         route_str = f'{route_str} - 0'
     if merge:
         print(route_str)  
-    print("Total Distance Covered by All Vehicle ",total_routeCovered)    
+    print("Total Distance Covered by All Vehicles ",total_routeCovered)    
      
 
 
@@ -81,45 +81,6 @@ def load_instance(json_file):
     else:
         print("Check Your File Path")
     return None
-
-
-def ind2route(individual, instance):
-    route = []
-    vehicle_capacity = instance['vehicle_capacity']
-    depart_due_time = instance['depart']['due_time']
-    # Initialize a sub-route
-    sub_route = []
-    vehicle_load = 0
-    elapsed_time = 0
-    last_customer_id = 0
-    for customer_id in individual:
-        # Update vehicle load
-        demand = instance[f'customer_{customer_id}']['demand']
-        updated_vehicle_load = vehicle_load + demand
-        # Update elapsed time
-        service_time = instance[f'customer_{customer_id}']['service_time']
-        return_time = instance['distance_matrix'][customer_id][0]
-        updated_elapsed_time = elapsed_time + \
-            instance['distance_matrix'][last_customer_id][customer_id] + service_time + return_time
-        # Validate vehicle load and elapsed time
-        if (updated_vehicle_load <= vehicle_capacity) and (updated_elapsed_time <= depart_due_time):
-            # Add to current sub-route
-            sub_route.append(customer_id)
-            vehicle_load = updated_vehicle_load
-            elapsed_time = updated_elapsed_time - return_time
-        else:
-            # Save current sub-route
-            route.append(sub_route)
-            # Initialize a new sub-route and add to it
-            sub_route = [customer_id]
-            vehicle_load = demand
-            elapsed_time = instance['distance_matrix'][0][customer_id] + service_time
-        # Update last customer ID
-        last_customer_id = customer_id
-    if sub_route != []:
-        # Save current sub-route before return if not empty
-        route.append(sub_route)
-    return route
 
 
 def merge_rules(rules):
@@ -340,7 +301,7 @@ def run_gavrptw(instance_name, unit_cost, init_cost, wait_cost, delay_cost, ind_
     best_ind = tools.selBest(pop, 1)[0]
     print(f'Best individual: {best_ind}')
     print(f'Fitness: {best_ind.fitness.values[0]}')
-    print_route(ind2route(best_ind, instance),instance)
+    print_route(individual_to_route_decoding(best_ind, instance),instance)
     print(f'Total cost: {1 / best_ind.fitness.values[0]}')
 
 
